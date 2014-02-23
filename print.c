f7f86e9f0d5da9b2af36d992e6a69cc11deb6852
srujank1 <ksrujan@msn.com>
2014-02-22 18:30:02 -0700

    Update print.c

--- print.c
+++ print.c
@@ -4,31 +4,38 @@ static void print_page_header(char source_name[], char date[]);
 
 void print_line(char line[], char source_name_to_print[], char date_to_print[])
 {
-    char save_ch;
-    char *save_chp = NULL;
-    static int line_count = MAX_LINES_PER_PAGE;
     
-    if (++line_count > MAX_LINES_PER_PAGE)
-    {
-        /* Missing Code Here */
-    }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
-    {
-        /* Missing Code Here */
-    }
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
-    /* Missing Code Here */
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
+	char save_ch;
+	char *save_chp = NULL;
+    	static int line_count = MAX_LINES_PER_PAGE;
+    
+    	if (++line_count > MAX_LINES_PER_PAGE)
+    		{
+        		print_page_header(source_name_to_print[], date_to_print[])
+   		}
+   	if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
+    		{
+			save_ch = line[MAX_PRINT_LINE_LENGTH -1];
+			*save_chp = line[MAX_PRINT_LINE_LENGTH -1]; 
+    		}
+    	if (save_chp == NULL)
+    		{
+        		fprintf(source_name[], "%s\n", line[]);
+    		}
+		
+    	
+	if (save_chp != NULL)
+    		{
+			fprintf(source_name[], "%s\n", *save-chp);
+			print_line(char line[], char source_name_to_print[], char date_to_print[])	
+    		}
 }
 static void print_page_header(char source_name[], char date[])
 {
-    static int page_number = 0;
     
-    /* Missing Code Here */
+	static int page_number = 0;
+   	line_count = 0;
+	fprintf(source_name[], "%s\t%d\n", date[], page_number); 	
+	page_number++; 
+	    
 }
