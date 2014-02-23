81371b6d0767c78740446d294cb39541291e5d08
srujank1 <ksrujan@msn.com>
2014-02-23 11:58:34 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "common.h"
 
-
-
-//FILE *out;
-int main (int argc, const char *argv[])
+FILE *out;
+int main (int argc, const char *argv[]) // MAIN FUNCTION
 {
     FILE *source_file;
     char source_name[MAX_FILE_NAME_LENGTH];
 
-int line_number=0;	//total lines
-time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    int date[6]={tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec};
+	int line_number=0;	//total lines
+	time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    //int date[6]={tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec};
+    char date[50];
+    strftime(date, sizeof(date)-1, "%m-%d-%Y %H:%M", tm);
+    printf("%s\n", date);
 
-printf("Enter the source file name\n");
-scanf("%s", &source_name);
+	printf("Enter the source file name\n");
+	scanf("%s", &source_name);
 
 
-    //out=fopen("OUTPUT.txt","w");
+    out=fopen("OUTPUT.txt","w");
 
     if((source_file=fopen(source_name, "r"))==NULL){
         puts("source could not be opened");
@@ -33,21 +33,22 @@ scanf("%s", &source_name);
         }
     }
     fclose(source_file);
-    
+    fclose(out);
 
 
     return 0;
 }
 
-/*void print_to_output(char output_line[]){
-    fprintf(out, output_line);
+void print_to_output(char output_line[], int line_num)// /This function prints out the line number with the line
+{
+    fprintf(out, "%d:\t%s", line_num, output_line);
+}
+void print_to_header(char yearetc[], int page, char name[])// This function only prints out the header
+{
+    fprintf(out,"\nPAGE:\t%d\t%s\t%s\n\n", page,name, yearetc);
 }
 
-void print_to_header(char yearetc[], int page, char name[]){
-    fprintf(out,"%s %d %s\n", yearetc, page, name);
-}*/
-
-BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
+BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])//  This function fgets to fill the variable “source_buffer”
 {
     char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
     char source_buffer[MAX_SOURCE_LINE_LENGTH];
@@ -55,9 +56,9 @@ BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
     line_number++;
 
     if(fgets(source_buffer, sizeof source_buffer, src_file)!=NULL){
-print_line(source_buffer, src_name, todays_date);
-//fprintf(out, "%d:\t%s",line_number, source_buffer);
-fputs(source_buffer, stdout); //prints out the file to console
+		print_line(source_buffer, src_name, todays_date);
+		//fprintf(out, "%d:\t%s",line_number, source_buffer);
+		fputs(source_buffer, stdout);     //prints out the file to console
         return (TRUE);
     }
     else
