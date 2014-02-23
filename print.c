48d49eb148c58d2988d5f06408094d9d33a6b586
srujank1 <ksrujan@msn.com>
2014-02-22 23:33:29 -0700

    Update print.c

--- print.c
+++ print.c
@@ -1,18 +1,21 @@
 #include "common.h"
 
 static void print_page_header(char source_name[], char date[]);
+static int line_count;
+FILE *out;
 
 void print_line (char line[], char source_name_to_print[], char date_to_print[])
 {
-    	FILE *out_file = fopen( source_name_to_print, 'w');
+    	//FILE *out_file = fopen( source_name_to_print, "w");
+	out=fopen("OUTPUT.txt","w");
 	
 	char save_ch;
 	char *save_chp = NULL;
-    	static int line_count = MAX_LINES_PER_PAGE;
+    	line_count = MAX_LINES_PER_PAGE;
     
     	if (++line_count > MAX_LINES_PER_PAGE)
     		{
-        		print_page_header( out_file, date_to_print);
+        		print_page_header( source_name_to_print, date_to_print);
    		}
    	if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
     		{
@@ -24,7 +27,7 @@ void print_line (char line[], char source_name_to_print[], char date_to_print[])
     		}
     	if (save_chp != NULL)
     		{
-        		fprintf(out_file, "%s\n", line);
+        		print_to_output( line);
 			line = save_chp;
 			
     		}
@@ -32,16 +35,28 @@ void print_line (char line[], char source_name_to_print[], char date_to_print[])
     		
 	if (save_chp == NULL)
     		{
-			fprintf(out_file, "%s\n", line);
+			print_to_output( line);
 				
     		}
+fclose(out);
 }
 static void print_page_header(char source_name[], char date[])
 {
     
 	static int page_number = 0;
+	//char num = page_number;
+	//strcat(date, source_name);
+	//strcat(date, num);
    	line_count = 0;
-	fprintf(out_file, "%s\t%d", date, page_number); 	
+	//fprintf(out_file, "%s %d", date, page_number); 
+	print_to_header( date, page_number, date);	
 	page_number++; 
 	    
+}
+void print_to_output(char output_line[]){
+    fprintf(out, output_line);
+}
+
+void print_to_header(char yearetc[], int page, char name[]){
+    fprintf(out,"%s %d %s\n", yearetc, page, name);
 }
